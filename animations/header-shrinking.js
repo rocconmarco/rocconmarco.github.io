f79c8b51a44d8cde1ab64b402e4e7cf48d1f9f8c
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-05-16 17:24:14 +0200

    Aggiunte animazioni header
    Ho fatto in modo che l'header si riduca quando scorro in basso, il logo cambia e diventa stilizzato e la parte destra segue senza rimpicciolirsi e si posiziona al centro del nuovo header rimpicciolito

---
+++ animations/header-shrinking.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    var navbarContainer = document.getElementById("navbar-container");
+    var logo = document.getElementById("logo");
+    var navbarRightSection = document.getElementById("navbar-right-section");
+
+    if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        navbarContainer.style.height = "100px";
+        logo.style.height = "100px";
+        logo.src = "assets/images/logo-black-reduced.svg";
+
+    } else {
+        navbarContainer.style.height = "170px";
+        logo.style.height = "100%";
+        logo.src = "assets/images/logo-black.svg";
+
+    }
+}
