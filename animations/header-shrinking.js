9e4a343e7f88b0d0865a4a2ae5310397f29133c3
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-05-21 14:58:15 +0200

    Aggiunto hamburger menu e reso la sezione bio responsive

--- animations/header-shrinking.js
+++ animations/header-shrinking.js
@@ -4,16 +4,18 @@ function scrollFunction() {
     var navbarContainer = document.getElementById("navbar-container");
     var logo = document.getElementById("logo");
     var navbarRightSection = document.getElementById("navbar-right-section");
+    var hamburgerImage = document.getElementById("hamburger-image");
 
     if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         navbarContainer.style.height = "100px";
         logo.style.height = "100px";
         logo.src = "assets/images/alternative-logo-reduced.svg";
+        hamburgerImage.style.height = "40px";
 
     } else {
         navbarContainer.style.height = "170px";
         logo.style.height = "100%";
         logo.src = "assets/images/alternative-logo.svg";
-
+        hamburgerImage.style.height = "60px"
     }
 }
